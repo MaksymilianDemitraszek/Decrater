e3dc1b6ff72774c4da9514228865416caf38a7d3
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-17 23:46:18 +0100

    fix

--- dec/settings.py
+++ dec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j6#(1#p)n-lsdm9@&9*y=*l*%r&(pawrlc5qqzq__zhvy$#l@r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
