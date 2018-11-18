94d2b83e394e289b1dca907cbeb94735b10ec1ff
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-18 15:50:19 +0100

    get limitation

--- pathLogger/urls.py
+++ pathLogger/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PathLoggerView.as_view())
+    path('', views.PathLoggerView.as_view()),
+    path('safe/', views.PathLoggerView.as_view()),
 ]
--- pathResolver/views.py
+++ pathResolver/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from pathResolver.models import ResolvedPath
+from pathLogger.models import PathBlock
 from pathResolver.serializers import ResolvedPathSerializer
 from rest_framework import generics
 
@@ -11,4 +12,11 @@ class PathResolverView(APIView):
         queryset = ResolvedPath.objects.impose()
         serializer = ResolvedPathSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class PathResolverViewSafe(APIView):
+    def get(self, request):
+        queryset = ResolvedPath.objects.all()
+        serializer = ResolvedPathSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
