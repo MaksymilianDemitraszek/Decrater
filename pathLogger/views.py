7b970eced3e86b492dc0617f4b0462489a5abfca
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-18 03:49:02 +0100

    f

--- pathLogger/views.py
+++ pathLogger/views.py
@@ -5,17 +5,14 @@ from rest_framework.generics import DestroyAPIView,CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from . import models
+import json
 
 from pathLogger import serializers
 
 class PathLoggerView(APIView):
     def post(self, request):
-
-        text_file = open("Output.txt", "w")
-        text_file.write(str(request.data))
-        text_file.close()
-
-        serializer = serializers.PathBlockSerializer(data=request.data)
+        data = json.dumps(request.data)
+        serializer = serializers.PathBlockSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
