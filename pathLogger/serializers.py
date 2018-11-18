fa42d6f34114f5581a6090cc7860bd68105ca72a
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-18 03:21:59 +0100

    event required change

--- pathLogger/serializers.py
+++ pathLogger/serializers.py
@@ -9,7 +9,7 @@ class QuakeEventSerializer(serializers.ModelSerializer):
 
 
 class PathBlockSerializer(serializers.ModelSerializer):
-    quakeEvents = QuakeEventSerializer(many=True)
+    quakeEvents = QuakeEventSerializer(many=True, required=False)
 
     class Meta:
         model = PathBlock
@@ -20,9 +20,9 @@ class PathBlockSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         path_block_data, quake_events = self._split_to_paht_and_events(**validated_data)
-
         delta = QuakeDelta.objects.create_delta_from_event_list(path_block_data['deviceId'], quake_events,)
         path_block = PathBlock.objects.create(quakeDelta=delta, **path_block_data)
+
         return path_block
 
     def _get_event_instances_list(self, event_data_list, parent_path):
--- pathResolver/views.py
+++ pathResolver/views.py
@@ -8,12 +8,14 @@ from . import models
 
 from pathLogger import serializers
 
-class PathLoggerView(APIView):
-    def post(self, request):
+
+class PathResolverView(APIView):
+    def get(self, request):
         serializer = serializers.PathBlockSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
 
