f3011e89c2756daec025f32d58a8a143157c56ac
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-18 13:52:12 +0100

    get limitation

--- pathResolver/views.py
+++ pathResolver/views.py
@@ -6,9 +6,9 @@ from pathResolver.serializers import ResolvedPathSerializer
 from rest_framework import generics
 
 
-class PathResolverView(generics.ListAPIView):
-    def get(self, request, last=0):
-        queryset = ResolvedPath.objects.impose()[last-1:]
+class PathResolverView(APIView):
+    def get(self, request):
+        queryset = ResolvedPath.objects.impose()
         serializer = ResolvedPathSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
