98af9232c02ee6b707cd6a62b4745656eee0a952
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2018-11-18 04:21:25 +0100

    f

--- pathLogger/views.py
+++ pathLogger/views.py
@@ -5,20 +5,19 @@ from rest_framework.generics import DestroyAPIView,CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from . import models
-import json
+import json, ast
 
 from pathLogger import serializers
 
 class PathLoggerView(APIView):
 
     def post(self, request):
-
-        string = request.data
-        return Response({'what i got' : string}, status=status.HTTP_200_OK)
-        # serializer = serializers.PathBlockSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+        data = request.data
+        data['quakeEvents'] = ast.literal_eval(data['quakeEvents']) #it should be in serializer
+        serializer = serializers.PathBlockSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
